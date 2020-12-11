54a8e644161c4194ca9ae5f91df831630cf4ee8c
armorkys <ar.morkys@gmail.com>
2020-12-11 17:59:47 +0200

    Final fixes on validation

--- app/src/main/java/com/example/restclientandroid/activities/HistoricalInputActivity.java
+++ app/src/main/java/com/example/restclientandroid/activities/HistoricalInputActivity.java
@@ -19,6 +19,7 @@ import com.example.restclientandroid.model.ConnectionModel;
 import java.time.Clock;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 import retrofit2.Call;
@@ -76,8 +77,8 @@ public class HistoricalInputActivity extends AppCompatActivity {
         });
     }
 
-    public boolean validDate(String date){
-        if(date.length()!=10){
+    public boolean validDate(String date) {
+        if (date.length() != 10) {
             Toast.makeText(this, "Must be in \"yyy-mm-dd\" format", Toast.LENGTH_SHORT).show();
             return false;
         }
@@ -85,28 +86,32 @@ public class HistoricalInputActivity extends AppCompatActivity {
         try {
             year = Integer.valueOf(date.substring(0, 4));
             month = Integer.valueOf(date.substring(5, 7));
-             day = Integer.valueOf(date.substring(8, 10));
-        } catch(NumberFormatException e){
+            day = Integer.valueOf(date.substring(8, 10));
+        } catch (NumberFormatException e) {
             e.printStackTrace();
             Toast.makeText(this, "Bad number format", Toast.LENGTH_SHORT).show();
             return false;
         }
-    if(year <= 2014 && month <=9 && day < 30){
-        Toast.makeText(this, "Date must be later than 2014-09-30", Toast.LENGTH_LONG).show();
-        return false;
-    }
+        if (year <= 2014 && month <= 9 && day < 30) {
+            Toast.makeText(this, "Date must be later than 2014-09-29", Toast.LENGTH_LONG).show();
+            return false;
+        }
 
-    if(year == null || month == null || day == null){
-        Toast.makeText(this, "Must be in \"yyy-mm-dd\" format", Toast.LENGTH_SHORT).show();
-        return false;
-    }
+        if (year == null || month == null || day == null) {
+            Toast.makeText(this, "Must be in \"yyy-mm-dd\" format", Toast.LENGTH_SHORT).show();
+            return false;
+        }
 
-    if(year < 2014 || year > 2020 || month < 1 || month > 12 || day < 1 || day > 31){
-        Toast.makeText(this, "Dates out of range", Toast.LENGTH_SHORT).show();
-        return false;
-    }
+        if (year < 2014 || year > 2020 || month < 1 || month > 12 || day < 1 || day > 31) {
+            Toast.makeText(this, "Dates out of range", Toast.LENGTH_SHORT).show();
+            return false;
+        } else if (date.charAt(4) != '-' && date.charAt(7) != '-') {
+            Toast.makeText(this, "Bad symbol used as date seperator", Toast.LENGTH_SHORT).show();
+            return false;
+        }
         return true;
     }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -122,6 +127,24 @@ public class HistoricalInputActivity extends AppCompatActivity {
         btnSearch = findViewById(R.id.btnHistorySearch);
         btnSearch.setOnClickListener(v -> {
             if(validDate(dateFrom.getText().toString()) && validDate(dateTo.getText().toString())){
+
+                String dateA = dateFrom.getText().toString();
+                String dateB = dateTo.getText().toString();
+               int year1 = Integer.parseInt(dateA.substring(0, 4));
+                int month1 = Integer.parseInt(dateA.substring(5, 7));
+                int day1 = Integer.parseInt(dateA.substring(8, 10));
+                GregorianCalendar gCalendar1 = new GregorianCalendar(year1, month1-1, day1);
+
+               int year2 = Integer.parseInt(dateB.substring(0, 4));
+                int month2 = Integer.parseInt(dateB.substring(5, 7));
+                int day2 = Integer.parseInt(dateB.substring(8, 10));
+                GregorianCalendar gCalendar2 = new GregorianCalendar(year2, month2-1, day2);
+
+                if(gCalendar1.after(gCalendar2)){
+                    Toast.makeText(this, "\"Date From\" higher value than \"Date To\"", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
                 String curName = currencySpinner.getSelectedItem().toString();
                 openHistoricResult(v, curName, dateFrom.getText().toString(), dateTo.getText().toString());
             }
